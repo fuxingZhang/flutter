b8b63b4a93917020b86c919b0d09a58b4c2b4794
fuxingZhang <502545703@qq.com>
2018-10-16 17:54:39 +0800

    优化图件弹窗对4寸小屏手机的展示效果

--- land/lib/components/land.dart
+++ land/lib/components/land.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
-import './zoomable_image.dart';
+// import './zoomable_image.dart';
 
 Widget getLand(BuildContext context,List list, String path) {
   List<TableRow> lists = [];
@@ -111,18 +111,56 @@ Future<Null> _showPicture(BuildContext context, String path) async {
     barrierDismissible: false, // user must tap button!
     builder: (BuildContext context) {
       return new AlertDialog(
-        title: new Text('承包地块示意图',style: new TextStyle(
-            color: Color(0xFF614120),
-          ),
+        title: Container(
+          padding: const EdgeInsets.symmetric(vertical: 6.0),
+          color: Color(0xFFEBDDBD),
+          child: new Row(
+            children:[
+              const SizedBox(width: 40.0),
+              // new Opacity(opacity: 0.0, 
+              //   child: IconButton(
+              //     padding: EdgeInsets.all(8.0),
+              //     icon: Icon(const IconData(0xe14c, fontFamily: 'MaterialIcons')),
+              //     onPressed: () {
+              //       Navigator.of(context).pop();
+              //     },
+              //   )
+              // ),
+              new Expanded(
+                // flex: 1,
+                child: new Text('承包地块示意图',
+                  textAlign: TextAlign.center,
+                  style: new TextStyle(
+                    color: Color(0xFF614120),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0
+                  ),
+                ),
+              ),
+              IconButton(
+                padding: EdgeInsets.fromLTRB(4.0, 4.0, 4.0, 4.0),
+                icon: Icon(const IconData(0xe14c, fontFamily: 'MaterialIcons'),
+                  size: 30.0,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ]
+          )
         ),
+        titlePadding: EdgeInsets.all(0.0),
         content: Container(
-          // symmetric 上下margin
+          // symmetric 上下margin,horizontal 水平，也就是左右
           // EdgeInsets.fromLTRB(24.0, 20.0, 24.0, 24.0) 从左、上、右、下
-          margin: const EdgeInsets.symmetric(vertical: 0.0),
+          margin: const EdgeInsets.all( 0.0),
+          padding: const EdgeInsets.all( 0.0),
           height: 240.0,
           width: 600.0,
+          // color: Color(0xFFEBDDBD),
           child: 
             PhotoViewInline(
+            // PhotoView(
               backgroundColor: Colors.white,
               imageProvider: NetworkImage('https://www.gisstack.xyz/v1/api/photo?path=$path'),
             )
@@ -133,17 +171,17 @@ Future<Null> _showPicture(BuildContext context, String path) async {
         ),
         contentPadding: EdgeInsets.all(0.0),
         actions: <Widget>[
-          new FlatButton(
-            child: new Text(
-              '关闭', 
-              style: new TextStyle(
-                fontSize: 18.0
-              ),
-            ),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
+          // new FlatButton(
+          //   child: new Text(
+          //     '关闭', 
+          //     style: new TextStyle(
+          //       fontSize: 18.0
+          //     ),
+          //   ),
+          //   onPressed: () {
+          //     Navigator.of(context).pop();
+          //   },
+          // ),
         ],
       );
     },
